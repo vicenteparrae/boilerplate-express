8a7ef1cf0f681537a616c40bd4ab6a71042f45d5
vicenteparrae <77460512+vicenteparrae@users.noreply.github.com>
2022-12-27 20:12:40 +0100

    Test005 - Use the .env File (#6)

--- myApp.js
+++ myApp.js
@@ -1,18 +1,21 @@
 let express = require('express');
 let app = express();
+app = require('dotenv').config()
 console.log("Hello World");
 
-
+//Old Exercises
 //app.get("/", function (req, res) => {res.sendFile(__dirname + "/views/index.html")});
-
-
 // Normal usage
 //app.use(express.static(__dirname + "/public"));
-
 // Assets at the /public route
 //app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", (req, res) => { res.json({ message: "Hello json" }) });
+if (process.env.MESSAGE_STYLE === "uppercase") {
+    let myMessage = "Hello json".toUpperCase();
+} else {
+    let myMessage = "Hello json";
+}
+app.get("/json", (req, res) => { res.json({ message: myMessage }) });
 
 
 
