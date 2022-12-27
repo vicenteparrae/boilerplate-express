f8ea4cff7a15e3bb49504a5273b606868d137b6b
vicenteparrae <77460512+vicenteparrae@users.noreply.github.com>
2022-12-27 19:03:01 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
     res.send("Hello Express");
 });
 
