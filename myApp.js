b79efb4f894a3d6fa3c9666a809caae9b3e8a629
vicenteparrae <77460512+vicenteparrae@users.noreply.github.com>
2022-12-27 18:51:04 +0100

    Added the functiontion;

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+app.get('/', function (req, res) {
+    res.send('Hello Express');
+});
 
 
 
@@ -37,4 +40,4 @@ console.log("Hello World");
 
 
 
- module.exports = app;
+module.exports = app;
