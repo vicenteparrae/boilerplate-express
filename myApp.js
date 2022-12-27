0c10aad02230bb20b6387bdfd5c8b6bbab22c358
vicenteparrae <77460512+vicenteparrae@users.noreply.github.com>
2022-12-27 19:52:55 +0100

    Adding a json response

--- myApp.js
+++ myApp.js
@@ -1,15 +1,17 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/view/index.html");
-});
+
+//Displaying the index.html
+//app.get("/", function (req, res) {res.sendFile(__dirname + "/view/index.html")});
 
 // Normal usage
-app.use(express.static(__dirname + "/public"));
+//app.use(express.static(__dirname + "/public"));
 
 // Assets at the /public route
-app.use("/public", express.static(__dirname + "/public"));
+//app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/json", (req, res) { res.json({ message: "Hello json" }) });
 
 
 
