0bc789eb8d1bfa36500f714f727cb0f0f3877d7e
vicenteparrae <77460512+vicenteparrae@users.noreply.github.com>
2022-12-27 19:45:14 +0100

    Test003 - Serve Static Assets (#4)

--- myApp.js
+++ myApp.js
@@ -2,8 +2,14 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/view/index.html");;
+    res.sendFile(__dirname + "/view/index.html");
 });
+
+// Normal usage
+app.use(express.static(__dirname + "/public"));
+
+// Assets at the /public route
+app.use("/public", express.static(__dirname + "/public"));
 
 
 
