8de973226fd1c4255c22a7298d4baff282331805
vicenteparrae <77460512+vicenteparrae@users.noreply.github.com>
2022-12-27 21:31:15 +0100

    Fix Merge Conflict

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ let app = express();
 //Excercises - Meet the Node console
 //console.log("Hello World");
 
-//Excercises - Start a Working Express Server & Serve an HTML File & 
+//Excercises - Start a Working Express Server & Serve an HTML File &
 //app.get("/", function (req, res) => {res.sendFile(__dirname + "/views/index.html")});
 
 //Excercises - Serve Static Assets
@@ -14,19 +14,24 @@ let app = express();
 // Assets at the /public route
 //app.use("/public", express.static(__dirname + "/public"));
 
-let response = "Hello json";
+//Excercises - Serve JSON on a Specific Route & Use the .env File
+//let response = "Hello json";
+//app.get('/json', (req, res) => {
+//
+//    var mySecret = process.env['MESSAGE_STYLE'];
+//
+//    if (mySecret === "uppercase") {
+//        res.json({ "message": response.toUpperCase() });
+//    } else {
+//        res.json({ "message": response });
+//    }
+//
+//});
 
-app.get('/json', (req, res) => {
-
-    var mySecret = process.env['MESSAGE_STYLE'];
-
-    if (mySecret === "uppercase") {
-        res.json({ "message": response.toUpperCase() });
-    } else {
-        res.json({ "message": response });
-    }
-
-});
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+})
 
 
 
