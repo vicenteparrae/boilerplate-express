70db905bd74251bf52e9553554560c4fb5c7b2fd
vicenteparrae <77460512+vicenteparrae@users.noreply.github.com>
2022-12-27 21:06:07 +0100

    Fixing the Notes for the Old Exercises

--- myApp.js
+++ myApp.js
@@ -1,28 +1,32 @@
-require('dotenv').config();
+//require('dotenv').config();
 let express = require('express');
 let app = express();
 
-//Old Exercises
+//Excercises - Meet the Node console
 //console.log("Hello World");
+
+//Excercises - Start a Working Express Server & Serve an HTML File & 
 //app.get("/", function (req, res) => {res.sendFile(__dirname + "/views/index.html")});
+
+//Excercises - Serve Static Assets
 // Normal usage
 //app.use(express.static(__dirname + "/public"));
 // Assets at the /public route
 //app.use("/public", express.static(__dirname + "/public"));
 
-let response = "Hello json";
-
-app.get('/json', (req, res) => {
-
-    var mySecret = process.env['MESSAGE_STYLE'];
-
-    if (mySecret === "uppercase") {
-        res.json({ "message": response.toUpperCase() });
-    } else {
-        res.json({ "message": response });
-    }
-
-});
+//Excercises - Serve JSON on a Specific Route & Use the .env File
+//let response = "Hello json";
+//app.get('/json', (req, res) => {
+//
+//    var mySecret = process.env['MESSAGE_STYLE'];
+//
+//    if (mySecret === "uppercase") {
+//        res.json({ "message": response.toUpperCase() });
+//    } else {
+//        res.json({ "message": response });
+//    }
+//
+//});
 
 
 
