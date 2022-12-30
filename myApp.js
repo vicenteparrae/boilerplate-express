33d58d7f1ef7f9807f6589c54c88d34bf8ae2a3b
vicenteparrae <77460512+vicenteparrae@users.noreply.github.com>
2022-12-30 15:44:11 +0100

    Use template literals to form a formatted string

--- myApp.js
+++ myApp.js
@@ -7,15 +7,25 @@ let app = express();
 //Building the middleware
 const middleware = (req, res, next) => {
     req.time = new Date().toString();
+    //Loging the wholw request
+    //console.log(req)
     next();
 };
 //caling the middleware when ever the app get /now
-app.get("/:word/echo", middleware, (req, res) => {
-    const { word } = req.params;
-    console.log(req.method + " " + req.path + " - " + req.ip + word)
-    res.json({ echo: word });
-});
+//app.get("/:word/echo", middleware, (req, res) => {
+//    const { word } = req.params;
+//    console.log(req.method + " " + req.path + " - " + req.ip + word)
+//   res.json({ echo: word });
+//});
 
+//Excercises - Get Query Parameter Input from the Client
+
+app.get("/name", middleware, (req, res) => {
+    var firstName = req.query.first;
+    var lastName = req.query.last;
+    console.log(req.method + " " + req.path + " - " + req.ip + " - " + JSON.stringify(req.query))
+    res.json({ name: firstName + " " + lastName });
+});
 
 //Excercises - Implement a Root-Level Request Logger Middleware
 //app.use((req, res, next) => {
